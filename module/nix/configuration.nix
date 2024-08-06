1b5899d18a4edca103e13be152d0a3f68af6c8a3
Jaan Tollander de Balsch <jaan@hey.com>
2024-08-06 11:47:10 +0300

    nix configuration

--- module/nix/configuration.nix
+++ module/nix/configuration.nix
@@ -1,6 +1,91 @@
-{ config, pkgs, callPackage }:
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, callPackage, ... }:
 
 {
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.initrd.luks.devices."luks-fad838ed-d1fb-4392-a982-6ec4454875b9".device = "/dev/disk/by-uuid/fad838ed-d1fb-4392-a982-6ec4454875b9";
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Helsinki";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "C.UTF-8";
+    LC_IDENTIFICATION = "C.UTF-8";
+    LC_MEASUREMENT = "C.UTF-8";
+    LC_MONETARY = "C.UTF-8";
+    LC_NAME = "C.UTF-8";
+    LC_NUMERIC = "C.UTF-8";
+    LC_PAPER = "C.UTF-8";
+    LC_TELEPHONE = "C.UTF-8";
+    LC_TIME = "C.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.jaan = {
+    isNormalUser = true;
+    description = "Jaan";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      bash
+      alacritty
+      neovim
+      tmux
+      gnupg
+      openssh
+      git
+      lsd
+      fd
+      fzf
+      bat
+      ripgrep
+      starship
+      firefox
+      keepassxc
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    bash
+    wget
+    neovim
+    pulseaudio
+    bluez
+  ];
+
   fonts.packages = with pkgs; [
     (nerdfonts.override { fonts = [ "Inconsolata" ]; })
   ];
@@ -30,4 +115,32 @@
   };
 
   programs.dconf.enable = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
 }
