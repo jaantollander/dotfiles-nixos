a1cbb4b19e5477c2d98b7a3a98e4dc0e3846de6a
Jaan Tollander de Balsch <jaan@hey.com>
2024-08-09 11:05:56 +0300

    enable rootless docker

--- configuration.nix
+++ configuration.nix
@@ -136,6 +136,12 @@
   # Configure video drivers
   services.xserver.videoDrivers = [ "modesetting" ];
 
+  # Enable rootless docker
+  virtualisation.docker.rootless = {
+    enable = true;
+    setSocketVariable = true;
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
